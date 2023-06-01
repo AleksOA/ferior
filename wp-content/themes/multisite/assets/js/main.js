53c1ab4f6356aec63b4181a9fcb985d0a874c39b
AleksOA <ovcharenko.alekss@gmail.com>
2023-06-01 18:44:23 +0300

    add data from Gform_1 to  Gform_3

--- wp-content/themes/multisite/assets/js/main.js
+++ wp-content/themes/multisite/assets/js/main.js
@@ -1,20 +1,36 @@
+// add data from Gform_1 to  Gform_3
 const btnLogin = document.getElementById('login_input_1_5');
-console.log(btnLogin)
+const siteURL_input_3_1 = document.getElementById('input_3_1');
+const siteName_input_3_3 = document.getElementById('input_3_3');
 
-const dataGform_1 = {
-    'siteURL': '',
-    'siteName': '',
-};
 function saveData(){
-    console.log('saveData')
     let siteURL = document.getElementById('input_1_9').value;
-    dataGform_1.siteURL= siteURL;
     let siteName = document.getElementById('input_1_10').value;
-    dataGform_1.siteName= siteName;
+
+    if(siteURL != null && siteURL != "" ) {
+        localStorage.setItem('siteURL', siteURL );
+    }
+    if(siteName != null && siteName != "" ) {
+        localStorage.setItem('siteName', siteName );
+    }
 }
 
 if(btnLogin != null) {
     btnLogin.addEventListener('click', saveData);
 }
+if(siteURL_input_3_1 != null){
+    let siteURL = localStorage.getItem('siteURL');
+    if(siteURL != null && siteURL != undefined) {
+        siteURL_input_3_1.value = siteURL;
+        localStorage.removeItem('siteURL');
+    }
+}
 
-console.log(dataGform_1);
+if(siteName_input_3_3 != null){
+    let siteName = localStorage.getItem('siteName');
+    if(siteName != null && siteName != undefined) {
+        siteName_input_3_3.value = siteName;
+        localStorage.removeItem('siteName');
+    }
+}
+// =================================================
